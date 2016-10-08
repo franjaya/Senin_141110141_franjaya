c3116097ee863c89f7912bf662c0dac458b3dd68
franjaya <franjayalim@gmail.com>
2016-10-08 19:57:11 +0700

    Senin_141110141_FranjayaLIM

---
+++ latihan_2_1/latihan_2_1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace latihan_2_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.domainUpDown2 = new System.Windows.Forms.DomainUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.SuspendLayout();
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Location = new System.Drawing.Point(87, 38);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.domainUpDown1.TabIndex = 0;
+            // 
+            // domainUpDown2
+            // 
+            this.domainUpDown2.Location = new System.Drawing.Point(258, 37);
+            this.domainUpDown2.Name = "domainUpDown2";
+            this.domainUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.domainUpDown2.TabIndex = 1;
+            this.domainUpDown2.SelectedItemChanged += new System.EventHandler(this.domainUpDown2_SelectedItemChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(413, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "tambah";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(504, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.CalendarDimensions = new System.Drawing.Size(2, 2);
+            this.monthCalendar1.Location = new System.Drawing.Point(76, 92);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 4;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 469);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.domainUpDown2);
+            this.Controls.Add(this.domainUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.DomainUpDown domainUpDown2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+    }
+}
+
---
+++ latihan_2_1/latihan_2_1/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace latihan_2_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DateTime mulai = new DateTime(2016, 1, 1);
+            DateTime akhir = new DateTime(2016, 12, 31);
+            while (mulai < akhir)
+            {
+                if (mulai.DayOfWeek == DayOfWeek.Saturday)
+                {
+                    monthCalendar1.AddBoldedDate(mulai);
+                    mulai = mulai.AddDays(1);
+                    if (mulai != akhir)
+                        monthCalendar1.AddBoldedDate(mulai);
+
+                    mulai = mulai.AddDays(5);
+                }
+                mulai = mulai.AddDays(1);
+            }
+            blan = new string[] { "januari", "februari", "maret", "april", "mei", "juni", "juli", "agustus", "september", "oktober", "november", "december" };
+            tanggalak = new int[] { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+           
+             for (int a = 1; a < tanggalak[0]; a++)
+            {
+                domainUpDown1.Items.Add(a);
+            }
+            for (int a = 0; a < 12; a++)
+                domainUpDown2.Items.Add(blan[a]);
+            domainUpDown1.SelectedItem = "1";
+            domainUpDown2.SelectedItem = "januari";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int buln = domainUpDown2.SelectedIndex + 1;
+            int tgl = domainUpDown1.SelectedIndex + 1;
+
+
+            monthCalendar1.AddAnnuallyBoldedDate(new DateTime(2016, buln,tgl));
+            monthCalendar1.UpdateBoldedDates();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            monthCalendar1.RemoveAllAnnuallyBoldedDates();
+            monthCalendar1.UpdateBoldedDates();
+
+
+        }
+        private int[] tanggalak;
+        private string[] blan;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            System.DateTime hitam = new DateTime(2016, 11, 26);
+            monthCalendar1.AddAnnuallyBoldedDate(hitam);
+            monthCalendar1.UpdateBoldedDates();
+           
+            monthCalendar1.AddAnnuallyBoldedDate(new DateTime(2016, domainUpDown2.SelectedIndex+1, domainUpDown1.SelectedIndex+1));
+            monthCalendar1.UpdateBoldedDates();
+        }
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+
+        }
+        
+        private void domainUpDown2_SelectedItemChanged(object sender, EventArgs e)
+        {
+            for (int a = 0; a < 12; a++)
+            {
+                if (domainUpDown2.SelectedItem == blan[a])
+                {
+                    domainUpDown1.Items.Clear();
+                    for (int b = 1; b <= tanggalak[a]; b++)
+                        domainUpDown1.Items.Add(b);
+
+                    domainUpDown1.SelectedItem = 1;
+                    break;
+                }
+            }
+        }
+    }
+}
